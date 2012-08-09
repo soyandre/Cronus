705010e4c6d4f7bf1c8499a63a940e46dbda68f5
Carlos Heitor Lain <atendimento@virtuati.com.br>
2012-08-09 18:04:57 -0300

    Pequena correção no código do upgrade 3446.sql

--- sql-files/upgrades/3446.sql
+++ sql-files/upgrades/3446.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `inventory`  ADD COLUMN `favorite` TINYINT UNSIGNED NOT NULL DEFAULT '0' AFTER `expire_time`;
+ALTER TABLE `inventory`  ADD COLUMN `favorite` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0' AFTER `expire_time`;
