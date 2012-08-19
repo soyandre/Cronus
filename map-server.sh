5129e487486f78713b2a2b149908dffdf4af53a2
Carlos Heitor Lain <atendimento@virtuati.com.br>
2012-08-18 21:35:25 -0300

    Remoção do Shell start (Não é mais necessário graças ao cronus-commander) Traduzido shell de char, login e map-server.

--- map-server.sh
+++ map-server.sh
@@ -5,9 +5,9 @@ ulimit -Sc unlimited
 
 while [ 1 ] ; do
 if [ -f .stopserver ] ; then
-echo server marked down >> servlog.txt
+echo servidor desligado >> servlog.txt
 else
-echo restarting server at time at `date +"%m-%d-%H:%M-%S"`>> startlog.txt
+echo reiniciando servidor em `date +"%m-%d-%H:%M-%S"`>> startlog.txt
 ./map-server
 fi
 
