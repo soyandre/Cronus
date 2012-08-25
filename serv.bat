91fd0d0ca6755e9af6c1b19f3ded63c0c1252be5
Marcelo Liberato <liberato.marcelo@hotmail.com>
2012-08-25 12:54:16 -0300

    Atualizado alguns DOCs

--- serv.bat
+++ serv.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
-IF "%1"=="" GOTO DIRECT
-IF "%2"=="" GOTO DIRECT
+IF "%1"=="" GOTO DIRETO
+IF "%2"=="" GOTO DIRETO
 IF NOT EXIST "%1" GOTO NOTFOUND
 GOTO START
 
@@ -16,7 +16,7 @@ REM Windows 95, 98, ME
 :RESTART_9X
 REM Old Ctrl+C in PING does not work, because that only stops ping,
 REM not the batch file.
-CHOICE /C:rc /N /T:R,15 Restarting in 15 seconds, press 'C' to cancel.
+CHOICE /C:rc /N /T:R,15 Reiniciando em 15 segundos, pressione 'C' para cancelar.
 IF NOT ERRORLEVEL 2 GOTO START
 GOTO END
 
@@ -24,35 +24,35 @@ REM Windows 2000, XP, Vista, 7
 :RESTART_NT
 REM There is no CHOICE in 2000 and XP, but you get asked whether to
 REM abort the batch file, when pressing Ctrl+C in PING.
-ECHO Restarting in 15 seconds, press Ctrl+C to cancel.
+ECHO Reiniciando em 15 segundos, pressione 'Ctrl+C' para cancelar.
 PING -n 15 127.0.0.1 > NUL
 
 :START
 %1
 ECHO.
 REM Return value > 1 is exception&~0xC0000000
-IF ERRORLEVEL 2 GOTO CRASHED
+IF ERRORLEVEL 2 GOTO ERRO
 REM Return value 1 is EXIT_FAILURE
 IF ERRORLEVEL 1 GOTO EXIT1
 REM Return value 0 is EXIT_SUCCESS
-ECHO %2 has shutdown successfully.
+ECHO %2 terminou com êxito.
 GOTO RESTART_NT
 
 :EXIT1
-ECHO %2 has terminated abnormally.
+ECHO %2 terminou anormalmente.
 GOTO RESTART_NT
 
-:CRASHED
-ECHO %2 has crashed!
+:ERRO
+ECHO %2 deu erro!
 GOTO RESTART_NT
 
-:DIRECT
-ECHO Do not run this file directly. It is used by logserv.bat, charserv.bat,
-ECHO mapserv.bat and their '-sql' counterparts.
+:DIRETO
+ECHO Não inicie este arquivo diretamente. Ele é usado pelo logserv-sql.bat,
+ECHO charserv-sql.bat e mapserv-sql.bat.
 GOTO END
 
 :NOTFOUND
-ECHO %1 was not found. Make sure, that you have compiled the %2.
+ECHO %1 não foi encontrado. Tenha certeza que %2 foi compilado.
 GOTO END
 
 :END
